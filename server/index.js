60ae67d2a78aa7db48189db164c63f8323154237
sankarshandev98 <gsankar164@gmail.com>
2022-12-04 02:37:04 +0530

    update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const socketIO = require("socket.io");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const users = [{}];
 
