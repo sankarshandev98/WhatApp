222e82a6a96cb8ccdf6da1ce50501944a44e5266
sankarshandev98 <gsankar164@gmail.com>
2022-12-04 02:38:50 +0530

    update

--- chat-app/src/component/Chat/Chat.js
+++ chat-app/src/component/Chat/Chat.js
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = "https://demo-cchat.herokuapp.com/";
+const ENDPOINT = "http://localhost:3001";
 
 const Chat = () => {
   const [id, setid] = useState("");
